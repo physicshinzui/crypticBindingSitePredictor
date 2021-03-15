fbc8b423034290bc3722752480864131a7f2c344
Shinji Iida <siida@Shinjis-MacBook-Air.local>
2021-03-15 14:23:28 +0900

    A new test case was added to test.sh.

--- test.sh
+++ test.sh
@@ -23,4 +23,9 @@ elif [ ${num} -eq 1 ]; then
     traj=/Volumes/siida-SSD/cryptic_project/bcl-xl/processed_xtc/protein-h_frm200ns/npt_prod_2.xtc
     ref=/Volumes/siida-SSD/cryptic_project/bcl-xl/ref/protein-h_renum.pdb
     crypticSitePredictor.py -r ${ref} -i ${traj} -o test2_file
+
+elif [ ${num} -eq 2 ]; then 
+    traj=/Volumes/siida-SSD/cryptic_project/integrin/processed_xtc/protein-h_frm200ns/for_test/all_skip100.xtc
+    ref=/Volumes/siida-SSD/cryptic_project/integrin/processed_xtc/protein-h_frm200ns/for_test/ref_protein-h.pdb
+    crypticSitePredictor.py -r ${ref} -i ${traj} -o test_integrin_file
 fi
